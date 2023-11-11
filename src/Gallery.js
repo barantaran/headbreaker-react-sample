f7add299781e74c5a43441c1576f69433204fabe
Dmitri Yourchev <d.yurchev@yandex.ru>
2023-11-12 03:09:43 +0600

    Fix gallery card click

--- src/Gallery.js
+++ src/Gallery.js
@@ -7,15 +7,8 @@ function Gallery({ setPuzzleIndex }) {
 
     return (
             Puzzles.map((puzzle, index) => (
-                // <div className="gallery-item" key={index}>
-                //     <img src={puzzle.thumb} alt={puzzle.name} />
-                //     <button type="button" onClick={()=>setPuzzleIndex(index)}>
-                //         Unpuzzle
-                //     </button>
-                //     {puzzle.name}
-                // </div>
-                <div className="col">
-                    <GalleryCard key={index} title={puzzle.name} image={puzzle.thumb} description={puzzle.description} link={puzzle.path} setPuzzleIndex={setPuzzleIndex}/>
+                <div className="col" onClick={()=>setPuzzleIndex(index)}>
+                    <GalleryCard key={index} title={puzzle.name} image={puzzle.thumb} description={puzzle.description} link={puzzle.path}/>
                 </div>
             ))
     );
--- src/GalleryCard.js
+++ src/GalleryCard.js
@@ -1,16 +1,12 @@
 
 export const GalleryCard = ({ title, image, description, link }) => {
     return (
-            <a href="https://www.mythrillfiction.com/the-dark-rider">
                 <div className="card">
                     <div className="wrapper">
-                        <img src="images\photorealistic_hell_landsape_200px.png" className="cover-image" alt="Mythrill" />
-                        {/* <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg" className="cover-image" alt="Mythrill" /> */}
+                        <img src="images/photorealistic_German_Shepherd_background_200.png" className="cover-image" alt="Mythrill" />
                     </div>
                     <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-title.png" className="title" alt="Mythrill" />
-                    <img src="images/photorealistic_face_of_ancient_daemon_character_200px.png" className="character" alt="Mythrill" />
-                    {/* <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp" className="character" alt="Mythrill" /> */}
+                    <img src="images/photorealistic_German_Shepherd_in_a_spacesuit_200_nbg.png" className="character" alt="Mythrill" />
                 </div>
-            </a>
     );
 };
