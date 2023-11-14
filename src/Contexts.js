1cd18a3144b252b480dc3eb934536db15d120886
Dmitri Yourchev <d.yurchev@yandex.ru>
2023-11-15 00:53:53 +0600

    Add poodle puzzle

--- src/Contexts.js
+++ src/Contexts.js
@@ -11,12 +11,12 @@ export const PuzzlesContext = createContext(
           HRURL: "https://storage.googleapis.com/jigsaw-puzzle-images/photorealistic_German_Shepherd_in_a_spacesuit_on_the_planet_2912.png",
         },
         {
-          name: "German Shepherd on a planet",
-          galleryBg: "images/photorealistic_German_Shepherd_background_200.png",
-          galleryChar: "images/photorealistic_German_Shepherd_in_a_spacesuit_200_nbg.png",
-          image: "images/photorealistic_German_Shepherd_in_a_spacesuit_700.png",
-          titleImage:"images/GermanShepherdTitle_200.png",
-          HRURL: "https://storage.googleapis.com/jigsaw-puzzle-images/photorealistic_German_Shepherd_in_a_spacesuit_on_the_planet_2912.png",
+          name: "Poodle astronaut in a cockpit",
+          galleryBg: "images/poodle_back_image_200.png",
+          galleryChar: "images/poodle_nbg_200.png",
+          image: "images/poodle_700.png",
+          titleImage:"images/PoodleTitle_200.png",
+          HRURL: "https://storage.googleapis.com/jigsaw-puzzle-images/poodle_astronaut.png",
         },
         {
           name: "German Shepherd on a planet",
